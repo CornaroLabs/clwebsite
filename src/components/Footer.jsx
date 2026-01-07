ce5a0c96bc2dec9bd1ae8fa44df25884e08541cf
CornaroLabs <info@cornarolabs.xyz>
2026-01-07 13:31:35 +0200

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -21,15 +21,7 @@ const Footer = () => {
         </ul>
 
         <div className="text-sm text-center space-y-3 max-w-xs lg:max-w-sm">
-          <div className="space-y-1 text-xs">
-            <p className="font-semibold">Locations</p>
-            <p>Nicosia, Cyprus</p>
-            <p>
-              UK: 4th Floor, Silverstream House, 45 Fitzroy Street, Fitzrovia, London,
-              W1T 6EB, GB
-            </p>
-          </div>
-
+    
           <p>
             For inquiries, please reach out to us at{" "}
             <a href="mailto:info@cornarolabs.xyz" className="text-[#00b9fa]">
